c48c55ba18e0868d43a211e64732bede3e058250
underlinear <arduouschore@gmail.com>
2023-08-18 22:33:09 +0800

    Add function to display pressed button

--- app.js
+++ app.js
@@ -26,3 +26,13 @@ function operate(a, b) {
 function doSomethingCool() {
     console.log("This function is temporarily out of order");
 }
+
+function displayInput (event) {
+    const display = document.querySelector('[type="text"]');
+    const pressedButton = event.target;
+    display.value += pressedButton.textContent;
+}
+
+const buttons = Array.from(document.querySelectorAll('button'));
+
+buttons.forEach((button)=>button.addEventListener('click',(e)=>displayInput(e)));
